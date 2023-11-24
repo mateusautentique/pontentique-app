6db4df92764216ab96b4d31fdcf66417694d0418
mateuszanella <mateusblk1@gmail.com>
2023-11-24 11:28:06 -0300

    Initial Commit

---
+++ Pontentique/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pontentique
+//
+//  Created by Mateus Zanella on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pontentique/PontentiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontentiqueApp.swift
+//  Pontentique
+//
+//  Created by Mateus Zanella on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PontentiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
