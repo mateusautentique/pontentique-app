617c8a89f21d5c742ef46616811ef67c9a97ce0f
Guilherme Luiz Cella <guilhermecella8@gmail.com>
2024-01-10 15:29:00 -0300

    remove and Final de semana and add Folga

--- Pontentique/Helpers/ClockTableRow.swift
+++ Pontentique/Helpers/ClockTableRow.swift
@@ -34,15 +34,17 @@ struct ClockTableRow: View {
                         VStack(alignment: .leading, spacing: 0) {
                             if !isWeekday(clockEntry.day) {
                                 HStack(alignment: .top, spacing: 0){
-                                        Text("Final de semana")
+                                    ForEach(0..<4){_ in
+                                        Text("Folga")
                                             .padding(7)
-                                            .frame(width: 255)
+                                            .frame(width: 60)
                                             .fixedSize(horizontal: false, vertical: true)
                                             .fixedSize()
-                                            .background(.white .opacity(0.8))
-                                            .foregroundColor(.black)
+                                            .background(.white .opacity(0.17))
+                                            .foregroundColor(.white . opacity(0.50))
                                             .cornerRadius(10)
                                             .padding(.trailing, 5)
+                                    }
                                 }
                             }else  if clockEntry.events.isEmpty && isWeekday(clockEntry.day) {
                                 HStack(alignment: .top, spacing: 0) {
@@ -85,128 +87,128 @@ struct ClockTableRow: View {
                         }
                     }
                 }
-                    .padding(0)
-                    .frame(alignment: .leading)
-                    
-                    Spacer()
-                    BalanceValue(balanceHours: $clockEntry.balanceHoursOnDay)
-                        .bold()
-                        .padding(.leading, 5)
-                        .padding(.trailing, 6)
-                        .frame(width: 60)
-                }
-                .background(ColorScheme.appBackgroudColor)
+                .padding(0)
+                .frame(alignment: .leading)
+                
+                Spacer()
+                BalanceValue(balanceHours: $clockEntry.balanceHoursOnDay)
+                    .bold()
+                    .padding(.leading, 5)
+                    .padding(.trailing, 6)
+                    .frame(width: 60)
             }
+            .background(ColorScheme.appBackgroudColor)
         }
     }
+}
+
+
+//MARK: - UTILS
+func isWeekday(_ dateString: String) -> Bool {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    guard let date = dateFormatter.date(from: dateString) else {
+        return false
+    }
+    let calendar = Calendar.current
+    let weekday = calendar.component(.weekday, from: date)
+    return !(weekday == 1 || weekday == 7)
+}
+func isEventToday(_ dateString: String) -> Bool {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    guard let date = dateFormatter.date(from: dateString) else {
+        return false
+    }
+    let comparisonResult = Calendar.current.isDate(date, equalTo: Date(), toGranularity: .day)
+    return comparisonResult
+}
+func isToday(_ timestamp: String) -> Bool {
+    guard let eventDate = convertToDate(timestamp) else {
+        return false
+    }
+    let comparisonResult = Calendar.current.isDate(eventDate, equalTo: Date(), toGranularity: .day)
+    return comparisonResult
+}
+
+func convertToDate(_ timestamp: String) -> Date? {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return dateFormatter.date(from: timestamp)
+}
+func dateFormat(_ timestamp: String) -> String {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "yyyy-MM-dd"
     
+    if let date = inputFormatter.date(from: timestamp) {
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "dd/MM"
+        return outputFormatter.string(from: date)
+    } else {
+        return "Invalid date"
+    }
+}
+
+func timeFormat(_ timestamp: String) -> (String) {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     
-    //MARK: - UTILS
-    func isWeekday(_ dateString: String) -> Bool {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        guard let date = dateFormatter.date(from: dateString) else {
-            return false
+    if let date = inputFormatter.date(from: timestamp) {
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "HH:mm"
+        return outputFormatter.string(from: date)
+    } else {
+        return "Invalid time"
+    }
+}
+
+func checkBalanceValue(_ duration: String) -> Bool {
+    let components = duration.split(separator: ":")
+    if let hours = Int(components[0]), let minutes = Int(components[1]) {
+        let totalMinutes = hours * 60 + minutes
+        return totalMinutes >= 0
+    } else {
+        return false
+    }
+}
+
+extension DateFormatter {
+    static let yyyyMMddHHmmss: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
+}
+
+extension Array {
+    func chunks(of size: Int) -> [[Element]] {
+        stride(from: 0, to: count, by: size).map {
+            Array(self[$0..<Swift.min($0 + size, count)])
         }
-        let calendar = Calendar.current
-        let weekday = calendar.component(.weekday, from: date)
-        return !(weekday == 1 || weekday == 7)
     }
-    func isEventToday(_ dateString: String) -> Bool {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        guard let date = dateFormatter.date(from: dateString) else {
-            return false
-        }
-        let comparisonResult = Calendar.current.isDate(date, equalTo: Date(), toGranularity: .day)
-        return comparisonResult
-    }
-    func isToday(_ timestamp: String) -> Bool {
-        guard let eventDate = convertToDate(timestamp) else {
-            return false
-        }
-        let comparisonResult = Calendar.current.isDate(eventDate, equalTo: Date(), toGranularity: .day)
-        return comparisonResult
-    }
-    
-    func convertToDate(_ timestamp: String) -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        return dateFormatter.date(from: timestamp)
-    }
-    func dateFormat(_ timestamp: String) -> String {
-        let inputFormatter = DateFormatter()
-        inputFormatter.dateFormat = "yyyy-MM-dd"
+}
+
+
+//MARK: - PREVIEW
+struct ClockTableRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let clockEntry: ClockEntry
+        @State var clockReport: ClockReport?
+        @State var endDate = Date()
+        @State var startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
         
-        if let date = inputFormatter.date(from: timestamp) {
-            let outputFormatter = DateFormatter()
-            outputFormatter.dateFormat = "dd/MM"
-            return outputFormatter.string(from: date)
-        } else {
-            return "Invalid date"
+        do {
+            let url = Bundle.main.url(forResource: "RowExampleData", withExtension: "json")!
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            decoder.dateDecodingStrategy = .formatted(DateFormatter.yyyyMMddHHmmss)
+            clockEntry = try decoder.decode(ClockEntry.self, from: data)
+        } catch {
+            print("Error decoding JSON: \(error)")
+            clockEntry = ClockEntry(day: "", normalHoursWorkedOnDay: "", extraHoursWorkedOnDay: "", balanceHoursOnDay: "", totalTimeWorkedInSeconds: 0, eventCount: 0, events: [])
         }
-    }
-    
-    func timeFormat(_ timestamp: String) -> (String) {
-        let inputFormatter = DateFormatter()
-        inputFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
-        if let date = inputFormatter.date(from: timestamp) {
-            let outputFormatter = DateFormatter()
-            outputFormatter.dateFormat = "HH:mm"
-            return outputFormatter.string(from: date)
-        } else {
-            return "Invalid time"
-        }
+        return ClockTableRow(clockEntry: clockEntry, clockReport: $clockReport, startDate: $startDate, endDate: $endDate)
     }
-    
-    func checkBalanceValue(_ duration: String) -> Bool {
-        let components = duration.split(separator: ":")
-        if let hours = Int(components[0]), let minutes = Int(components[1]) {
-            let totalMinutes = hours * 60 + minutes
-            return totalMinutes >= 0
-        } else {
-            return false
-        }
-    }
-    
-    extension DateFormatter {
-        static let yyyyMMddHHmmss: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            return formatter
-        }()
-    }
-    
-    extension Array {
-        func chunks(of size: Int) -> [[Element]] {
-            stride(from: 0, to: count, by: size).map {
-                Array(self[$0..<Swift.min($0 + size, count)])
-            }
-        }
-    }
-    
-    
-    //MARK: - PREVIEW
-    struct ClockTableRow_Previews: PreviewProvider {
-        static var previews: some View {
-            let clockEntry: ClockEntry
-            @State var clockReport: ClockReport?
-            @State var endDate = Date()
-            @State var startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
-            
-            do {
-                let url = Bundle.main.url(forResource: "RowExampleData", withExtension: "json")!
-                let data = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                decoder.dateDecodingStrategy = .formatted(DateFormatter.yyyyMMddHHmmss)
-                clockEntry = try decoder.decode(ClockEntry.self, from: data)
-            } catch {
-                print("Error decoding JSON: \(error)")
-                clockEntry = ClockEntry(day: "", normalHoursWorkedOnDay: "", extraHoursWorkedOnDay: "", balanceHoursOnDay: "", totalTimeWorkedInSeconds: 0, eventCount: 0, events: [])
-            }
-            
-            return ClockTableRow(clockEntry: clockEntry, clockReport: $clockReport, startDate: $startDate, endDate: $endDate)
-        }
-    }
+}
